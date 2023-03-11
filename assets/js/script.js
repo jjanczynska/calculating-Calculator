e14a5d99435cba4ecc9d6a6745b65ab9da976876
jjanczynska <joaxjan@gmail.com>
2023-03-11 15:22:55 +0000

    Add function inputing operator buttons into the calculator screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,5 +5,17 @@ let buttons = document.getElementsByClassName("buttons");
 // adding "click" event listeners to clicked buttons
 
 buttons.addEventListener("click", (function(e) {
-    let target = e.target
+    let target = e.target;
+// function that inputs pressed number button into the calculator screen
+    if(target.classList.contains(number)) {
+        result.value += target.value;
+    }
+// function that inputs pressed operator button into the calculator screen
+if(target.classList.contains(operator)) {
+    result.value += " " + target.value;
+}
+// function deleting the calculator screen with C button
+if(target.classList.conatins("clear")) {
+    result.value = " ";
+}
 }))
