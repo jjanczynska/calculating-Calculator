0f0e2ec2fc39b53927bd7ce0964664550fab4d56
jjanczynska <joaxjan@gmail.com>
2023-03-11 15:13:44 +0000

    Add onclick event listener and function that inputs number buttons to calculator screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
 // declaring variables for easier operations
 let result = document.getElementById("result");
 let buttons = document.getElementsByClassName("buttons");
+
+// adding "click" event listeners to clicked buttons
+
+buttons.addEventListener("click", (function(e) {
+    let target = e.target
+}))
