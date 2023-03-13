69c35bdc36be35b0ec5e0265cc678e84850dac04
jjanczynska <joaxjan@gmail.com>
2023-03-13 13:00:11 +0000

    Declare numbers from calculator buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,5 +10,15 @@ const minus = document.getElementById("minus");
 const plus = document.getElementById("add");
 const equals = document.getElementById("equal");
 // number buttons
+const zero = document.getElementById("zero");
+const one = document.getElementById("one");
+const two = document.getElementById("two");
+const three = document.getElementById("three");
+const four = document.getElementById("forur");
+const five = document.getElementById("five");
+const six = document.getElementById("six");
+const seven = document.getElementById("seven");
+const eight = document.getElementById("eight");
+const nine = document.getElementById("nine");
 
 
